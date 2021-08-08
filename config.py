7c7d97a89f5bbe47d5282cc8bf762b2b74a02dda
Bmaili <61977609+Bmaili@users.noreply.github.com>
2021-08-08 21:48:07 +0800

    信息配置模块

---
+++ config.py
@@ -0,0 +1,20 @@
+## 基础部分
+# 填写你的学号
+stutendID = 'XXXX'
+
+# 填写你的密码
+passward = 'XXXX'
+
+
+## 通知部分,打卡失败进行邮件通知
+# 是否选择开启邮件通知功能,True则开启,并填写下面信息
+isSendMessage = False
+
+# 设置自己接收打卡信息通知的邮箱
+receiver = 'XXXX@qq.com'
+
+# 发送的邮箱，这里可以设置成与接收邮箱一致（即自己发给自己），或者也可以用其他的
+sender = receiver
+
+# 填写发送者的QQ邮箱SMTP授权码
+mailPass = 'XXXX'

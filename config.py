e459999443bdc974768def242200435b83fb4ffc
Devil-Z <superheaoz@hotmail.com>
2022-04-27 16:28:53 +0800

    修改通知逻辑，支持Server酱

--- config.py
+++ config.py
@@ -1,20 +1,27 @@
-## 基础部分
+### 基础部分
 # 填写你的学号
-stutendID = 'XXXX'
+stutendID = ''
 
 # 填写你的密码
-passward = 'XXXX'
+passward = ''
 
 
-## 通知部分,打卡失败进行邮件通知
-# 是否选择开启邮件通知功能,True则开启,并填写下面信息
-isSendMessage = False
+### 通知部分,打卡失败进行邮件通知
+# 当相应渠道设置后，将自动推送通知
 
-# 设置自己接收打卡信息通知的邮箱
-receiver = 'XXXX@qq.com'
+## 邮件通知
+# 设置自己接收打卡信息通知的邮箱(XXXX@qq.com)
+receiver = ''
 
 # 发送的邮箱，这里可以设置成与接收邮箱一致（即自己发给自己），或者也可以用其他的
 sender = receiver
 
-# 填写发送者的QQ邮箱SMTP授权码
-mailPass = 'XXXX'
+# 填写发送邮箱SMTP授权码/密码
+mailPass = ''
+
+# 填写发送邮箱的SMTP服务器地址(smtp.qq.com)
+mailHost = ''
+
+## ServerChan通知
+# Server酱的SCKEY,可以在Server酱的网站注册获取
+sckey = ''
